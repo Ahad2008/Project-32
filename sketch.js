38360610825bd1987a300fd60661835382900279
Ahad2008 <72267510+Ahad2008@users.noreply.github.com>
2020-12-30 15:56:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -160,19 +160,19 @@ function keyPressed(){
   }
 }
 async function getBackgroundImage(){
-   var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-   var responseJSON = await response.json();
+  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
 
-   var datetime = responseJSON.datetime;
-   var hour = datetime.slice(11, 13);
-   //console.log(hour);
+  var datetime = responseJSON.datetime;
+  var hour = datetime.slice(11, 13);
+  //console.log(hour);
 
-   if (hour >= 06 && hour <= 18) {
-     bg = "light.jpg";
-   } else {
-     bg = "dark.jpg";
-   }
+  if (hour >= 06 && hour <= 18) {
+    bg = "light.jpg";
+  } else {
+    bg = "dark.jpg";
+  }
 
-   backgroundImg = loadImage(bg);
-   //console.log(backgroundImg);
+  backgroundImg = loadImage(bg);
+  console.log(backgroundImg);
 }
